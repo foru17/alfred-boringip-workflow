53888f07f46022cb65397ae93edc960912afbed1
luolei <meleilei@gmail.com>
2015-09-09 15:42:29 +0800

    增加源文件

--- bundle/getip.php
+++ bundle/getip.php
@@ -1,6 +1,6 @@
 <?php
+
 require_once('workflows.php');
-define('IP_QUERY_URL','http://ip.taobao.com/service/getIpInfo.php?ip=');
 $w = new Workflows();
 $query = '{query}';
 if (strpos($query, '://') === false) {
@@ -8,15 +8,18 @@ if (strpos($query, '://') === false) {
 }
 $host = parse_url($query);
 $domain = $host['host'];
+
 function getIp($q){
-    $ipNum = gethostbyname($q);
-    return $ipNum;
+	$ipNum = gethostbyname($q);
+	return $ipNum;
 }
+
 $ip = getIp($domain);
 if(strpos($query,$ip) == true){
-    $ip ='未找到IP地址';
+	$ip ='IP not found';
 }
-$w->result('lookupdomainip', $ip,'IP地址是:'. $ip , '搜索域名:'. $domain, 'icon.png', 'yes');
+$w->result('lookupdomainip', $ip,'IP Address:'. $ip , 'Domain:'. $domain, 'icon.png', 'yes');
 echo $w->toxml();
 
-?>
+
+ ?>
