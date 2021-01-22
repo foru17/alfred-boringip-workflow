14c4363f0f407c8664862d41516def7a8ce229d0
luolei <meleilei@gmail.com>
2021-01-22 11:22:30 +0800

    增加安装包

--- README.md
+++ README.md
@@ -1,21 +1,24 @@
-
 #### About
 
 调试网络的时候经常需要查看域名解析的IP，过去习惯用`ping`，写了 Alfred 插件，支持快捷解析域名IP。
 
-#### 使用方法
+#### Feature
+
+* 通过域名查找IP
+* 查找IP信息
+* 获得到IP地址的延迟
+
+#### Usage 
 
 * 需要 Alfred Powerpack 支持,请支持正版。
 
 下载[lookupdomainip.alfredworkflow](https://github.com/foru17/alfred-lookupdomainip/blob/master/lookupdomainip.alfredworkflow),安装。
 
-![](https://luo.is26.com/source/images/cip1.jpg)
+![](https://static.is26.com/source/github/boringip-1.JPG)
 
-调起 Alfred，输入`cip {你要查找的 URL 即可}`
+调起 Alfred，输入`bip {你要查找的 URL 或者IP地址即可}`
 
-* 可直接输入完整 URL，会自动解析出域名。
-
-![](https://luo.is26.com/source/images/cip2.jpg)
+* 可直接输入完整 URL，会自动解析出域名IP。
 
 回车即可复制 IP 地址到剪贴版。
 
--- cip.php
+++ cip.php
@@ -9,63 +9,79 @@ const ICON = 'icon.png';
 
 $wf = new Workflow;
 
-function getIPData($ipr) {
-    $response = request('https://free.is26.com/api/v1/getIp/' . urlencode($ipr) );
+function getIPData($ipr)
+{
+    $response = request('https://free.is26.com/api/v1/getIp/' . urlencode($ipr));
     $json = json_decode($response);
     $result = $json->data;
     return $result;
 }
 
-function getIp($q) {
+function getIp($q)
+{
     $ipNum = gethostbyname($q);
     return $ipNum;
 }
 
-if (filter_var($query, FILTER_VALIDATE_IP)) {
-    $response = request('https://free.is26.com/api/v1/getIp/' . urlencode($query) );
+if (filter_var($query, FILTER_VALIDATE_IP))
+{
+    $response = request('https://free.is26.com/api/v1/getIp/' . urlencode($query));
     $json = json_decode($response);
     $result = $json->data;
     $ping = new Ping($result->ip);
     $latency = $ping->ping();
-
+    $msTag = ' ms';
     $wf->result()
         ->title($result->isp . ' ' . $result->area . ' ' . $result->region . ' ' . $result->city . ' ' . $result->country)
-        ->subtitle($result->country_id . ' 延迟 ' . $latency)
-        ->arg($result->isp . ' ' . $result->region . ' ' . $result->city . ' ' . $result->country)
+        ->subtitle($result->country_id . ' 延迟 ' . $latency . $msTag)->arg($result->isp . ' ' . $result->region . ' ' . $result->city . ' ' . $result->country)
         ->icon(ICON)
         ->autocomplete($key);
-        
+
     echo $wf->output();
 
 }
-else {
-    if (strpos($query, '://') === false) {
+else
+{
+    if (strpos($query, '://') === false)
+    {
         $query = 'http://' . $query;
     }
-    $host = parse_url($query);
-    $domain = $host['host'];
 
-    $ip = getIp($domain);
-    $latencyTag = ' 延迟';
-    $response = request('https://free.is26.com/api/v1/getIp/' . urlencode($ip) );
-    $json = json_decode($response);
-    $result = $json->data;
+    $file_headers = @get_headers($query);
+    if (!$file_headers || $file_headers[0] == 'HTTP/1.1 404 Not Found')
+    {
+        $exists = false;
+    }
+    else
+    {
+        $host = parse_url($query);
+        $domain = $host['host'];
 
-    if (strpos($query, $ip) == true) {
-        $ip = 'IP not found';
-        $latencyTag = '';
+        $ip = getIp($domain);
+        $latencyTag = ' 延迟';
+        $msTag = ' ms';
+        $response = request('https://free.is26.com/api/v1/getIp/' . urlencode($ip));
+        $json = json_decode($response);
+        $result = $json->data;
+
+        if (strpos($query, $ip) == true)
+        {
+            $ip = 'IP not found';
+            $latencyTag = '';
+            $msTag = '';
+        }
+
+        $ping = new Ping($ip);
+        $latency = $ping->ping();
+
+        $wf->result()
+            ->title($result->isp . ' ' . $result->area . ' ' . $result->region . ' ' . $result->city . ' ' . $result->country)
+            ->subtitle($ip . ' ' . $result->country_id . $latencyTag . $latency . $msTag)->arg($ip)->icon(ICON)
+            ->autocomplete($key);
+
+        echo $wf->output();
+
     }
 
-    $ping = new Ping($ip);
-    $latency = $ping->ping();
-
-    $wf->result()
-        ->title($result->isp . ' ' . $result->area . ' ' . $result->region . ' ' . $result->city . ' ' . $result->country)
-        ->subtitle($ip . ' ' . $result->country_id . $latencyTag . $latency)
-        ->arg($ip)
-        ->icon(ICON)
-        ->autocomplete($key);
-
-    echo $wf->output();
 }
 
